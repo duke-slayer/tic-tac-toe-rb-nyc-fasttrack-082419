962cecd866ed3ab5239e9ed818453c85ca8d4b49
Amy Duke <amyyyduke@gmail.com>
2019-08-25 18:21:15 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,75 @@
+# Helper Methods
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+# Define your play method below
+def play(board)
+  i = 0 
+  until i == 9 
+  turn (board)
+  i += 1 
+end
+end
+
+def turn_count (board)
+  x = 0 
+  board.each do |position|
+    if position == "X" || position == "O"
+      x += 1 
+    end
+  end 
+  return x 
+end 
+
+#objective is to deduce whose turn it is, leveraging the fact that we know how many turns have happened already 
+def current_player(board)
+    if turn_count(board) % 2 == 0 || turn_count(board) == 0 
+      return "X"
+    else
+      return "O"
+    end
+  end 
+ 
+
+
+def play(board)
+  turn(board) until over?
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elseif draw?(board)
+    puts "Cat's Game!"
+  end
+end 
